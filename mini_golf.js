a63b1d0b2c303d5be0172ae61a83c0d5e10dfd84
Jasper Holton <jasper@holton.io>
2021-04-14 17:11:58 -0700

    Creating simple two player mini golf game

---
+++ mini_golf.js
@@ -0,0 +1,42 @@
+function init() {
+  var id = document.getElementById("gameid").innerHTML;
+  var last = 0;
+  var stage = new createjs.Stage("game156");
+  stage.canvas.height = window.innerHeight - 54;
+  green = new createjs.Shape();
+  green.graphics.beginFill("green").drawRect(0, 0, window.innerWidth, window.innerHeight);
+  stage.addChild(green);
+  createjs.Touch.enable(stage);
+  /*stage.on("stagemousedown", function(evt) {
+    playerball = new createjs.Shape();
+    playerball.graphics.beginFill("red").drawCircle(0, 0, 20);
+    playerball.x = evt.x
+    
+    console.log("https://uglek.com/game/" + id + "/post/")
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST", "https://uglek.com/game/" + id + "/post/", true);
+    xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded; charset=UTF-8");
+    xhr.send("put 90 50");
+  });*/
+  
+  start = new createjs.Shape();
+  start.graphics.beginFill("grey").drawRect(0, 0, 100, 100);
+  start.x = 0;
+  start.y = 0;
+  start.on("stagemousedown", function(evt) {
+    playerball = new createjs.Shape();
+    playerball.graphics.beginFill("white").drawCircle(0, 0, 5);
+    playerball.x = evt.stageX;
+    playerball.y = evt.stageY;
+    stage.addChild(playerball)
+  });
+  
+    //Update stage will render next frame
+  stage.update();
+  createjs.Ticker.setFPS(60);
+  createjs.Ticker.addEventListener("tick", stage);
+  createjs.Ticker.addEventListener("tick", handleTick);
+  function handleTick(event) {
+    stage.update();
+  }
+}
