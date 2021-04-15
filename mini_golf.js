f7e810a52b76ac799ae257c8de11fa7e7de42c52
Jasper Holton <jasper@holton.io>
2021-04-14 17:58:55 -0700

    Update mini_golf.js

--- mini_golf.js
+++ mini_golf.js
@@ -33,19 +33,6 @@ var movestarty;
       playerball.graphics.beginFill("white").drawCircle(0, 0, 5);
       playerball.x = evt.stageX;
       playerball.y = evt.stageY;
-      playerball.on("pressmove", function(evt) {
-        if(!pressmovestarted){
-          movestartx = evt.stageX;
-          movestarty = evt.stageY;
-        }
-      });
-      playerball.on("pressup", function(evt) {
-          movex = movestartx - evt.stageX;
-          movey = movestartx - evt.stageY;
-          playerball.x = playerball.x + movex;
-          playerball.y = playerball.y + movex;
-        pressmovestarted = false;
-      });
       stage.addChild(playerball)
       ballplaced = true;
     }
@@ -55,6 +42,13 @@ var movestarty;
     }
   });
   stage.addChild(start);
+
+stage.on("stagemouseup", function(evt) {
+          movex = playerball.x - evt.stageX;
+          movey = playerball.y - evt.stageY;
+          playerball.x = playerball.x + movex;
+          playerball.y = playerball.y + movex;
+      });
   
     //Update stage will render next frame
   stage.update();
