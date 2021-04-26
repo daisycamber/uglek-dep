ceba66aebfdecbd8558d31582b02aaab1411694e
Jasper Holton <jasper@holton.io>
2021-04-26 13:58:39 -0700

    Update mini_golf.js

--- mini_golf.js
+++ mini_golf.js
@@ -8,7 +8,7 @@ background = new createjs.Shape();
 stage.addChild(container);
 less = window.innerWidth;
 if(window.innerHeight < less){
-  less = window.innerHeight;
+  less = window.innerHeight - 54;
 }
  scale = container.scale = less/1000;
 stage.canvas.width = window.innerWidth;
