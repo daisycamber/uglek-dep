98616fbb02e6311bcab564e25526ed9e788813ea
Jasper Holton <jasper@holton.io>
2022-02-09 15:21:16 -0800

    Update threethirteen.js

--- threethirteen.js
+++ threethirteen.js
@@ -1,4 +1,4 @@
-// By Jasper Camber Holton. V0.1.0126 - Recovering gameplay state - Even more fixes
+// By Jasper Camber Holton. V0.1.0126 - Recovering gameplay state - Even more fixes again
 (function threethirteen(){
   const TURNTIME = 5; // Turn time in seconds
   var currentTurn = 0;
