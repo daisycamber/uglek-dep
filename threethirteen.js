94732965dfb6fb79471a7b8722aebe21cbf49bb7
Jasper Holton <jasper@holton.io>
2022-09-02 21:50:55 -0700

    Fixing dupes,

--- threethirteen.js
+++ threethirteen.js
@@ -759,7 +759,7 @@ function stringDeck(deck) {
     }
     firstdiscard = deck[cr*2 + 1].Value
     firstdiscardsuit = deck[cr*2 + 1].Suit
-    currentCard = cr*2 + 1 + 1;
+    currentCard = cr*2 + 1 + 2;
 
 
 
@@ -987,7 +987,7 @@ var lastDiscard;
       }
     });
   }
-  var currentCard = currentRound*2 + 1 + 1;
+  var currentCard = currentRound*2 + 1 + 2;
 
   function drawCardFromDeck(){
     if(canPlayerDraw){
